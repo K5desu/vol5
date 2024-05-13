340d6f55c46f8cbfb1b64075bbfbcc7b7ad74ed9
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-05-13 18:55:31 +0900

    error_handle

--- src/components/sidenav/sidenav.tsx
+++ src/components/sidenav/sidenav.tsx
@@ -32,7 +32,7 @@ export default function SideNav() {
               マイページ
             </Link>
           ) : (
-            <Login>{false}</Login>
+            <Login></Login>
           )}
         </div>
       </div>
