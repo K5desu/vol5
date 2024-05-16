513baaa51c83a4f7e1abed2a8304698266400ade
Kouki Kanayama <kouki@kurapiko.com>
2024-05-16 22:14:50 +0900

    ディレクトリ変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import { usePathname } from "next/navigation";
 import NextAuthProvider from "@/providers/NextAuth";
 import "@/app/globals.css";
-import SideNav from "@/components/sidenav/sidenav";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,12 +24,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider>
-          <div className="flex flex-row h-screen">
-            {!noSidebarPages.includes(pathname) && <SideNav />}
-            <div className={noSidebarPages.includes(pathname) ? "flex-1" : "flex-1 ml-64"}>
+            
               {children}
-            </div>
-          </div>
+            
         </NextAuthProvider>
       </body>
     </html>
