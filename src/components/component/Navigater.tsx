6311b09ce0f6db317f31f9192895e6221d8d47f9
Kouki Kanayama <kouki@kurapiko.com>
2024-05-18 20:14:37 +0900

    色々しました

--- src/components/component/Navigater.tsx
+++ src/components/component/Navigater.tsx
@@ -2,13 +2,25 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+    onScrollToPersonalityCards: () => void;
+  }
+
+const Navbar: React.FC<NavbarProps> = ({ onScrollToPersonalityCards }) => {
   return (
-    <nav style={{ background: '#333', padding: '10px', color: 'white' }}>
-      <Link href="/">使ってみる</Link>
-      <Link href="/">ストレスタイプ</Link>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex space-x-4">
+          <Link href="/" className="text-white hover:text-gray-400">
+            ホーム
+          </Link>
+          <a href="#" className="text-white hover:text-gray-400" onClick={onScrollToPersonalityCards}>ストレスタイプ診断</a>
+          
+        </div>
+      </div>
     </nav>
   );
 };
 
 export default Navbar;
+
---
+++ src/components/component/showPersonalityCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PersonalityCard from '@/components/component/TypeCard';
+import { types } from '@/data/types';
+
+interface ShowPersonalityCardProps {
+  typeName: string;
+}
+
+const ShowPersonalityCard: React.FC<ShowPersonalityCardProps> = ({ typeName }) => {
+  const selectedType = types.find((type) => type.title === typeName);
+
+  if (!selectedType) {
+    return <p>Type not found</p>;
+  }
+
+  return (
+    <PersonalityCard
+      title={selectedType.title}
+      description={selectedType.description}
+      color={selectedType.color}
+      tag={selectedType.tag}
+      animationData={selectedType.animationData}
+    />
+  );
+};
+
+export default ShowPersonalityCard;
