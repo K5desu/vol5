e761ca46455e872a24732b431654692f2c08949b
Kouki Kanayama <kouki@kurapiko.com>
2024-05-18 18:40:12 +0900

    導入ページの仮完成、選択肢によって、ストレスタイプを呼び出せるように設定

--- src/components/component/intro-card-ui.tsx
+++ src/components/component/intro-card-ui.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../shadcn/ui/card_f_intro'
+import React from 'react';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../shadcn/ui/card_f_intro';
 
 interface ICardProps {
-    title: string;
-    description: string;
-    content: string;
-    footer: string;
-  }
-
-  const ICard: React.FC<ICardProps> = ({ title, description, content, footer }) => {
-  return (
-    <Card className='bg-white'>
-  <CardHeader>
-    <CardTitle>{title}</CardTitle>
-    <CardDescription>{description}</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>{content}</p>
-  </CardContent>
-  <CardFooter>
-    <p>{footer}</p>
-  </CardFooter>
-</Card>
-
-  )
+  title: string;
+  description: string;
+  content: string;
+  footer: string;
 }
 
-export default ICard
+const ICard: React.FC<ICardProps> = ({ title, description, content, footer }) => {
+  return (
+    <Card className="bg-white w-96 h-64 flex flex-col justify-between">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 overflow-auto">
+        <p>{content}</p>
+      </CardContent>
+      <CardFooter>
+        <p>{footer}</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ICard;
